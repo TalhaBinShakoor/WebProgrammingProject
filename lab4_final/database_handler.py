81ad9830bbae3e0952b4db599265a94ce6844a85
Talha Bin Shakoor <talta651@student.liu.se>
2021-03-18 23:26:56 +0100

    Lab4-Final

---
+++ lab4_final/database_handler.py
@@ -0,0 +1,229 @@
+import sqlite3
+from flask import g
+from uuid import uuid4
+import random
+import hashlib
+
+
+DATABASE_URI = "/home/mutab736/web_programming/tddd97/lab3/twidder"      # Mutaz's Machine Path
+DATABASE_URI = "D:\\WebDevelopment\\WebLab\\lab4\\tddd97\\lab4_final\\twidder" # Talha's Machine Path
+
+
+# This method is responsible for establishing the database connection with SQLite. 
+def get_db():
+    db = getattr(g, 'db', None)
+    if db is None:
+        db = g.db = sqlite3.connect(DATABASE_URI)
+    return db
+
+
+# This method is responsible for disconnect the existing connection. 
+def disconnect_db():
+    db = getattr(g, 'db', None)
+    if db is not None:
+        g.db.close()
+        g.db = None
+
+
+# This method is responsible for signin functionality of database. It checks the cradentials of a user with database. If succes? It returns the random generated tocken for the session. else: it raise exception. 
+def sign_in(email, password):
+    currentPassword = get_db().execute(
+        "SELECT password FROM User WHERE email =?;", [email])
+    currentPassword = convert_cursor_with_single_value_to_string(
+        currentPassword)
+    currentPassword = str(currentPassword)
+    password = hashlib.sha3_256(password.encode()).hexdigest()
+    if password == currentPassword:
+        rand_token = str(uuid4())
+        rand_token = generate_token() #rand_token[0:rand_token.find("-")]
+        status = "logedIn"
+        get_db().execute("insert into UserSession(token,user_email,status) values (?,?,?);",
+               (rand_token, email, status))
+        get_db().commit()
+        return rand_token
+    else:
+        raise Exception('Incorrect Password')
+
+
+# This method is responsible for Signup functionality of database. It insert the cradentials of a user in database.
+def sign_up(email, first_name, last_name, gender, city, country,telephone, password):
+    get_db().execute("insert into User (email, first_name, last_name, gender, city, country,telephone_no, password) values (?,?,?,?,?,?,?,?)",
+           (email, first_name, last_name, gender, city, country,telephone, password))
+    get_db().commit()
+    return True
+
+
+# This method is responsible for Signout functionality of database. It checks the status of a user in database. If 'logdIn'? It changes the status into 'logOut'
+def sign_out(userToken):
+    user_status = get_db().execute(
+        "SELECT status FROM UserSession WHERE token =?;", [userToken])
+    user_status = convert_cursor_with_single_value_to_string(user_status)
+    user_status = str(user_status)
+    if str(user_status) == 'logedIn':
+        user_status = 'logedOut'
+        get_db().execute("UPDATE UserSession SET status ='" +
+               user_status+"' where token ='"+userToken+"';")
+        get_db().commit()
+        return True
+    else:
+        print("logIn First")
+        return False
+
+
+# This method is responsible for change password functionality of database. Firstly, It checks the status of a user in database. User enter old password and that is compared to the existing password(in Database). If both password matches? Password changed succesfuly. Else: password is incorrect.
+def change_password(userToken, oldPassword, newPassword):
+    user_status = get_db().execute(
+        "SELECT status FROM UserSession WHERE token =?;", [userToken])
+    user_status = convert_cursor_with_single_value_to_string(user_status)
+    user_status = str(user_status)
+    if str(user_status) == 'logedIn':
+        currentPassword = get_db().execute(
+            "SELECT password FROM User u inner join UserSession s on s.user_email=u.email WHERE token =?;", [userToken])
+        currentPassword = convert_cursor_with_single_value_to_string(
+            currentPassword)
+        currentPassword = str(currentPassword)
+        
+        userEmail = get_db().execute(
+            "SELECT email FROM User u inner join UserSession s on s.user_email=u.email WHERE token =?;", [userToken])
+        userEmail = convert_cursor_with_single_value_to_string(
+            userEmail)
+        userEmail = str(userEmail)
+        
+        oldPassword = hashlib.sha3_256(oldPassword.encode()).hexdigest()
+        #currentPassword = hashlib.sha3_256(currentPassword.encode()).hexdigest()
+        newPassword = hashlib.sha3_256(newPassword.encode()).hexdigest()
+
+
+        if oldPassword == currentPassword:
+            try:
+                get_db().execute("UPDATE User SET password=? where email=?;", [newPassword,userEmail])
+                get_db().commit()
+                return "success"
+            except Exception as e:
+                return False
+        else:
+            return "password is incorrect"
+    else:
+        return "Login First"
+
+
+# This method is responsible for getting data of the user from the database using token.  
+def get_user_data_by_token(userToken):
+    user_status = check_token_validaty(userToken)
+    if str(user_status) == 'logedIn':
+        user_profile = get_db().execute(
+            "SELECT u.* FROM User u inner join UserSession s on u.email = s.user_email  WHERE token =?;", [userToken])
+        return user_profile.fetchall()
+    elif str(user_status) == 'logedOut':
+        print("logIn First")
+        raise Exception("invalid token / logIn First")
+
+
+# This method is responsible for getting data of the user from the database using token and email.  
+def get_user_data_by_email(userToken, userEmail):
+    user_status = check_token_validaty(userToken)
+    if str(user_status) == 'logedIn':
+        user_data = get_db().execute("SELECT u.email,u.first_name, u.last_name, u.city, u.country, u.gender FROM User u WHERE u.email =?;", [userEmail])
+        return user_data.fetchall()
+    else:
+        raise Exception("invalid token / logIn First")
+
+
+# This method is responsible for getting all the messages of the user from the database using token.  
+def get_user_message_by_token(userToken):
+    user_status = check_token_validaty(userToken)
+    if str(user_status)=='logedIn':
+        user_message = get_db().execute("SELECT p.post_message FROM Post p inner join UserSession s on p.user_email = s.user_email  WHERE token=?;", [userToken])
+        return user_message.fetchall()
+    else:
+        raise Exception("invalid token / logIn First")
+
+# This method is responsible for getting all the messages of the user from the database using token and email.  
+def get_user_message_by_email(userToken,userEmail):
+    user_status = check_token_validaty(userToken)
+    if str(user_status)=='logedIn':
+        user_message = get_db().execute("SELECT post_message FROM Post WHERE user_email =?;", [userEmail])
+        return user_message.fetchall()
+    else:
+        raise Exception("invalid token / logIn First")
+
+
+# This method is responsible for adding the post messgaes of the user into the database using token and email.  
+def post_message(userToken, email,post_message,):
+    user_status = get_db().execute("SELECT status FROM UserSession WHERE token=? and user_email=?;", [userToken,email])        
+    user_status = convert_cursor_with_single_value_to_string(user_status)
+    user_status = str(user_status)
+    if user_status == 'logedIn':
+        get_db().execute("INSERT INTO Post(user_email,post_message) values(?,?);", [email,post_message])
+        get_db().commit()
+        return True;
+    else:
+        raise Exception("invalid token / logIn First")
+
+# This method is responsible for getting the user email from the database using token.  
+def get_user_email_by_token(userToken):
+    cursor = get_db().execute("SELECT user_email FROM UserSession where status = 'logedIn' and  token = ?", [userToken])
+    user = cursor.fetchone()
+    cursor.close()
+    if  user == None:
+        return False
+    else:
+        return user[0]
+
+# This method is responsible for getting the user data from the database using email.  
+def get_user_data_by_email_only(userEmail):
+        user_data = get_db().execute("SELECT u.email,u.first_name, u.last_name, u.city, u.country, u.gender, u.telephone_no FROM User u WHERE u.email =?;", [userEmail])
+        result = user_data.fetchall()
+        if result is None or len(result)==0:
+            raise Exception("Email dosen't exist")
+        else:
+            return result
+
+
+# **************** Utility Methods *******************
+
+# this method is responsible for converting the cusror (databese field/column) into a string.
+def convert_cursor_with_single_value_to_string(cursor):
+    fetch = cursor.fetchall()
+    if len(str(fetch))==2:
+        return "invalid token"
+    result = str(fetch[0])
+    result=str(result)[2:(len(result)-3)]
+    return result.strip()
+
+# this method is responsible for checking the validity of a token.
+def check_token_validaty(token):
+    token_status = get_db().execute("SELECT status FROM UserSession WHERE token =?;", [token])
+    token_status = convert_cursor_with_single_value_to_string(token_status)
+    token_status = str(token_status)
+    return token_status
+
+# this method is responsible for generating the random token.
+def generate_token():
+    TOKEN_LENGTH = 20
+    TOKEN_STRING = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ"
+    token = ""
+
+    for i in range(0, TOKEN_LENGTH):
+        token += TOKEN_STRING[random.randint(0, len(TOKEN_STRING)-1)]
+
+    return token
+
+# this method is responsible for getting status of the seesion through email.
+def get_session_status_by_email(userEmail):
+    token_status = get_db().execute("SELECT status FROM UserSession WHERE status='logedIn' and user_email =?;", [userEmail])
+    token_status = convert_cursor_with_single_value_to_string(token_status)
+    token_status = str(token_status)
+    if(token_status !="invalid token"):
+        return True
+    return False
+
+# this method is responsible for deactivating the status of the seesion using email.
+def deactivate_session_status(userEmail):
+    get_db().execute("UPDATE UserSession SET status='logedOut' where user_email=?;", [userEmail])
+    get_db().commit()
+
+# this method is responsible for Updating the password using email.
+def update_user_password(password,userEmail):
+    get_db().execute("UPDATE User SET password=? where email=?;", [password,userEmail])
+    get_db().commit()
---
+++ lab4_final/server.py
@@ -0,0 +1,337 @@
+from geventwebsocket.handler import WebSocketHandler
+from gevent.pywsgi import WSGIServer
+from geventwebsocket import WebSocketError
+
+from flask import Flask, jsonify, request
+app = Flask(__name__, static_url_path='')
+app.debug = True
+
+import database_handler
+import json
+from uuid import uuid4
+import logging
+import clx.xms
+import requests
+import random
+import string
+import hashlib
+
+sockets = dict()
+
+# initialization the logger
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('server.log')
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+
+# This is the root method that will return the static website
+@app.route('/')
+def root():
+    return app.send_static_file('client.html')
+
+# This method is responsible for closing the database connection.
+@app.teardown_request
+def after_request(exception):
+    database_handler.disconnect_db()
+
+# This method is responsible for bidirectional communication between client and server. We are using the websockets here because we want only one user to be active at a time. If a user will try to login from a diffrent browser then the old session will be discarded automatically.
+@app.route('/socket')
+def web_socket():
+    if request.environ.get('wsgi.websocket'):
+        ws = request.environ['wsgi.websocket']
+        obj = ws.receive()
+        data = json.loads(obj)
+
+        try:
+            sockets[data['email']] = ws
+
+            while True:
+                obj = ws.receive()
+                if obj is None:
+                    del sockets[data['email']]
+                    ws.close()
+                    return ""
+
+        except WebSocketError as e:
+            del sockets[data['email']]
+
+    return ""
+
+# This method is responsible fot signup process. For sending the password to the server the hashing is also implemented here for security.
+@app.route('/signup', methods=['POST'])
+def SignUp():
+    data = request.get_json()
+    if 'email' in data and 'firstname' in data and 'familyname' in data and 'gender' in data and 'country' in data and 'city' in data and 'telephone' in data and 'password' in data:
+        try:
+            hashed_password = hashlib.sha3_256(data['password'].encode()).hexdigest()
+            result = database_handler.sign_up(data['email'], data['firstname'], data['familyname'], data['gender'], data['country'], data['city'] ,data['telephone'], hashed_password)
+            if (result == True):
+                return "", 201   #User created sucessfully
+            else:
+                return "", 500  #Something went wrong on database
+        except Exception as e:
+            if(str(e.args[0])=='UNIQUE constraint failed: User.email'):
+                return "", 409   #Email already exist.
+            else:
+                logging.debug("Error4 while signup ",e)
+                return "", 500  #Something went wrong
+
+    else:
+        return "", 400 #Not good data.
+
+# This method is responsible fot signin process. For sending the password to the server the hashing is also implemented for security.
+@app.route('/signin', methods=['POST'])
+def SignIn():
+    data = request.get_json()
+    if 'email' in data and 'password' in data:
+        try:
+            is_user_loggedIn(data['email'])
+            result = database_handler.sign_in(data['email'], data['password'])
+            return jsonify({"token": result}), 200
+        except Exception as e:
+            print("Exception ",e)
+            if(str(e.args[0])== 'Incorrect Password'):
+                return "", 401 #Incorrect Password
+            else:
+                return "", 500 #Something went wrong.
+    else:
+        return "", 400 #Not good data
+
+
+# This method is responsible fot signout process. It checks the token. If token exists? it will be signout.  
+@app.route('/signout', methods=['GET'])
+def SignOut():
+    data = request.headers
+    if 'Token' in data:
+        result = database_handler.sign_out(data['Token'])
+        if (result == True):
+            return "", 200 #User Signout Sucessfully.
+        else:
+            return "", 401 #invalid token / Login First.
+    else:
+        return "", 400 #Not good data.
+
+
+
+# This method is responsible fot change password process. It checks the token as well as comapre the old password to the existing password. If token exists + Passwords matched? password will be changes sucessfuly. 
+@app.route('/changepassword', methods=['POST'])
+def ChangePassword():
+    h_data = request.headers
+    data = request.get_json()
+    if 'Token' in h_data and 'oldPassword' in data and 'newPassword' in data:
+        result = database_handler.change_password(h_data['Token'], data['oldPassword'], data['newPassword'])
+        if (result == "success"):
+            return "", 201 #Password Changed Sucessfully
+        elif(result == "password is incorrect"):
+            return "", 401 #Your Password is inncorrect
+        elif(result == "Login First"):
+            return "", 401  #You are logged out please login first.
+        else:
+            return "", 500 #Something went wrong
+
+    else:
+        return "", 400 #Not good data.
+
+
+# This method is responsible for fetching the data of a user by using token. It checks the token in data. If token exists? it will returns the user data of that coresponding token. 
+@app.route('/getuserdatabytoken', methods=['GET'])
+def GetUserDataByTocken():
+    data = request.headers
+    if 'Token' in data:
+        try:
+            result = database_handler.get_user_data_by_token(data['Token'])
+            x = {
+                "email": result[0][0],
+                "first_name": result[0][1],
+                "last_name": result[0][2],
+                "gender": result[0][3],
+                "Country": result[0][4],
+                "City": result[0][5]
+                }
+            result=json.dumps(x)
+            return result, 200
+        except Exception as e:
+            return "", 401 #invalid token
+    else:
+        return "", 400 #Not good data
+
+
+# This method is responsible for fetching the data of a user by using token + email. It checks the token and email in data. If token and email matches? it will returns the user data of that coresponding token+email.
+@app.route('/getuserdatabyemail', methods=['POST'])
+def GetUserDataByEmail():
+    h_data =request.headers
+    data = request.get_json()
+    if 'Token' in h_data and 'email' in data:
+        try:
+            result = database_handler.get_user_data_by_email(h_data['Token'], data['email'])
+
+            x = {
+                "email": result[0][0],
+                "first_name": result[0][1],
+                "last_name": result[0][2],
+                "gender": result[0][3],
+                "Country": result[0][4],
+                "City": result[0][5]
+                }
+            result=json.dumps(x)
+            return result, 200
+    
+        except Exception as e:
+            if(str(e.args[0])=='invalid token / logIn First'):
+                return "", 401 #Inavlid token / please login first.
+            return "", 500 #Server side probelm
+
+    else:
+        return "", 400 #Not good data
+
+
+# This method is responsible for fetching all the messages of a user by using token. It checks the token in data. If token exists? it will returns all the messages of that coresponding user.
+@app.route('/getusermessagebytoken', methods=['GET'])
+def GetUserMessageByToken():
+    data = request.headers
+    if 'Token' in data:
+        try:
+            result = database_handler.get_user_message_by_token(data['Token'])
+            result = convert_resultset_to_Json(result)
+     
+            result=json.dumps(result)
+            return result, 200
+    
+        except Exception as e:
+            if(str(e.args[0])=='invalid token / logIn First'):
+                return "", 401  #Inavlid token / please login first.
+            elif (str(e.args[0])=='Token is Invalid'):
+                return "", 401 #Tocken is Invalid.
+            return "", 500  #Something went wrong.
+    else:
+        return "", 400 #Not good data
+
+
+# This method is responsible for fetching all the messages of a user by using email + token. It checks the token and email in data. If token exists+email matched? it will returns all the messages of that coresponding user.
+@app.route('/getusermessagebyemail', methods=['POST'])
+def GetUserMessageByEmail():
+    h_data = request.headers
+    data = request.get_json()
+    if 'Token' in h_data and 'email' in data:
+        try:
+            result = database_handler.get_user_message_by_email(h_data['Token'], data['email'])
+            result = convert_resultset_to_Json(result)
+            result=json.dumps(result)
+            if(len(result)==2):
+                return jsonify(""),204
+            return result, 200
+    
+        except Exception as e:
+            if(str(e.args[0])=='invalid token / logIn First'):
+                return "", 401 #Inavlid token / please login first
+            return "", 500 #Something went wrong
+    else:
+        return "", 400  #Not good data.
+
+
+# This method is responsible for posting the message of a user onto the database by using email + token. It checks the token and email in data. If token exists (means: session is active) + email matched? it will post the message of that coresponding user onto the database.
+@app.route('/postmessage', methods=['POST'])
+def PostMessage():
+    h_data = request.headers
+    data = request.get_json()
+    if 'Token' in h_data and 'email' in data and 'message' in data:
+        try:
+            result = database_handler.post_message(h_data['Token'], data['email'], data['message'])
+            
+            return "", 201  #Message posted successfully
+    
+        except Exception as e:
+            if(str(e.args[0])=='invalid token / logIn First'):
+                return "", 401  #Inavlid token / please login first
+            return "", 500 #Something went wrong.
+    else:
+        return "", 400 #Not good data
+
+# This method is responsible for posting the message of a user onto the database by using email + token. It checks the token and email in data. If token exists (means: session is active) + email matched? it will post the message of that coresponding user onto the database.
+@app.route('/forgetpassword', methods=['POST'])
+def ForgetPassword():
+    h_data =request.headers
+    data = request.get_json()
+    if  'email' in data:
+        try:
+            logger.debug('@server.forgetPassword() :token and email values '+h_data['Token']+data['email'])
+            result = database_handler.get_user_data_by_email_only(data['email'])
+            logger.debug('@server.forgetPassword() :database result '+str(len(result)))
+            telephone_no = result[0][6]
+            generated_password = get_random_password(10)
+            original_generated_password = generated_password
+            generated_password = hashlib.sha3_256(generated_password.encode()).hexdigest()
+            database_handler.update_user_password(generated_password,data['email']) 
+            sms_result = send_sms(telephone_no,original_generated_password)
+
+            if sms_result==False:
+                return "",400  # 
+            return "", 200
+    
+        except Exception as e:
+            if(str(e.args[0])=="Email dosen't exist"):
+                return "", 204 
+            return "", 500 #Server side probelm
+
+    else:
+        return "", 400 #Not good data
+
+# **************** Utility Methods *******************
+# This method is responsble for removing the unwanted chracters from the end of the string. 
+def remove_dummy_chars_from_the_end_of_string(result):
+    result=str(result)[2:(len(result)-4)]
+    return result.strip()
+
+
+# This method is responsble for converting the result (fields) of database into JSON format. 
+def convert_resultset_to_Json(resultSet):
+    json_array=[]
+    for i in resultSet:
+        x = {
+                "message": remove_dummy_chars_from_the_end_of_string(i)
+                }
+        json_array.append(x)
+    return json_array
+
+# This method is responsble for checking the status that user is 'logdin' or 'logedout'. 
+def is_user_loggedIn(userEmail):
+    loggedIn = database_handler.get_session_status_by_email(userEmail)
+    if loggedIn ==True:
+        if userEmail in sockets:
+            try:
+                ws = sockets[userEmail]
+                ws.send(json.dumps({'success': False, 'message': 'You have been logged out'}))
+            except WebSocketError as e:
+                del sockets[userEmail]
+            database_handler.deactivate_session_status(userEmail)#remove_logged_in_user(database_helper.get_logged_in_user_by_email(email)[1])
+
+
+# This method is responsble for generating the random password. 
+def get_random_password(length):
+    # choose from all lowercase letter
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+# This method is responsble for sending the SMS to the user's phone number.
+def send_sms(phoneNo,message):
+    client = clx.xms.Client(service_plan_id='74900228354a470a8fcaef0c42e7b9f6', token='e49b712396684159ab34da77ec4b21da')
+    create = clx.xms.api.MtBatchTextSmsCreate()
+    create.sender = "Twidder"
+    to = {str(phoneNo)}
+    create.recipients = set((phoneNo, ))
+    create.body = "your new Password "+message
+
+    try:
+        batch = client.create_batch(create)
+        return True
+    except (requests.exceptions.RequestException,clx.xms.exceptions.ApiException) as ex:
+        logging.debug('Failed to communicate with XMS: %s' % str(ex))
+        return False
+
+if __name__ == '__main__':
+    http_server = WSGIServer(('',5001), app, handler_class=WebSocketHandler)
+    http_server.serve_forever()
+    #app.run()
